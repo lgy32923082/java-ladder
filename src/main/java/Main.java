698d5465a1bb2f5b0fde1e1ca5a2d98b789a58ab
geonyoon <lgy32923081@gmail.com>
2019-11-10 17:01:19 +0900

    Main과 필요없는 txt파일을 제거하였습니다.

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import controller.LadderController;
+
+public class Main {
+    public static void main(String[] args) {
+        LadderController ladderController = new LadderController();
+        ladderController.run();
+    }
+}
